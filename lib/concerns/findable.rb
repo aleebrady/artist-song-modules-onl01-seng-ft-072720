cd7f4d3427bdd6840ff013c2818ed66901c7d6fc
Aaron Brady <aleebrady@gmail.com>
2020-08-14 17:48:31 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,5 +1,5 @@
-module Findable
-  def find_by_name(name)
-    all.find { |n| n.name == name }
+module Findable # extended module to both classes 
+  def find_by_name(name) 
+    all.find { |n| n.name == name } 
   end
 end
